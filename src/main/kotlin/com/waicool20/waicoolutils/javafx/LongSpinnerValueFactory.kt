5cfd27c67fe25fbf9af6d571fdaf7e9289b48e27
waicool20 <waicool20@gmail.com>
2018-12-20 19:10:28 +0800

    Implement value factory for Long value spinners

---
+++ src/main/kotlin/com/waicool20/waicoolutils/javafx/LongSpinnerValueFactory.kt
@@ -0,0 +1,67 @@
+/*
+ * GPLv3 License
+ *
+ *  Copyright (c) WAI2K by waicool20
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.waicool20.waicoolutils.javafx
+
+import com.waicool20.waicoolutils.RequiresTornadoFX
+import javafx.scene.control.SpinnerValueFactory
+import tornadofx.*
+
+@RequiresTornadoFX
+class LongSpinnerValueFactory(
+        min: Long,
+        max: Long,
+        initialValue: Long = min,
+        amountToStepBy: Long = 1
+) : SpinnerValueFactory<Long>() {
+    val minProperty = min.toProperty()
+    val maxProperty = max.toProperty()
+    val amountToStepByProperty = amountToStepBy.toProperty()
+
+    var min by minProperty
+    var max by maxProperty
+    var amountToStepBy by amountToStepByProperty
+
+    override fun increment(steps: Int) {
+        val newIndex = value + steps * amountToStepBy
+        value = if (newIndex >= min) {
+            newIndex
+        } else {
+            if (isWrapAround) wrapValue(newIndex) else min
+        }
+    }
+
+    override fun decrement(steps: Int) {
+        val newIndex = value - steps * amountToStepBy
+        value = if (newIndex >= min) {
+            newIndex
+        } else {
+            if (isWrapAround) wrapValue(newIndex) else min
+        }
+    }
+
+    private fun wrapValue(v: Long): Long {
+        val r = v % max
+        return if (min in (max + 1)..(r - 1) || min in (r + 1)..(max - 1)) {
+            r + max - min
+        } else {
+            r
+        }
+    }
+}
