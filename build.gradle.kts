625a8e48464388094d4d4ce17fe20e072e6ba5f2
waicool20 <waicool20@gmail.com>
2021-06-24 20:11:13 +0800

    Update libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.0"
+    kotlin("jvm") version "1.5.10"
     id("org.openjfx.javafxplugin") version "0.0.8"
 }
 
